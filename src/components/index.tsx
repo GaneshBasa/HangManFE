b8c96af746539445d317ac463f9c3a3e7f4cc80f
Ganesh Basa <basaganesh1@gmail.com>
2024-06-19 15:03:38 +0530

    WIP

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,11 +1,18 @@
 import { FC, useEffect } from 'react'
 import { Card } from '@mui/material'
 
-import { be } from '@src/common/config'
+import { be } from '@common/config'
 
 
 const IndexComponent : FC = () => {
-  useEffect( () => {}, [] )
+  useEffect( () => {
+    fetch( be.game + '/' + 1 )
+    .then( res => {
+      if( res.ok ) return res.json()
+    } )
+    .then( console.log )
+    .catch( console.error )
+  }, [] )
 
   return <Card />
 }
