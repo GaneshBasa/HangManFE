ba42b152b0ab991261f28e8026f6597afe318ed1
Ganesh Basa <basaganesh1@gmail.com>
2024-06-19 12:54:10 +0530

    Added pages component

---
+++ src/components/common/page.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import { Box, Container } from '@mui/material'
+import { FC, ReactNode } from 'react'
+
+interface Props {
+  title: string
+  children?: ReactNode
+}
+
+const Page: FC<Props> = ({ title, children }) => (
+  <>
+    <Head>
+      <title>{title}</title>
+    </Head>
+    <Box
+      component='main'
+      sx={{
+        flexGrow: 1,
+        py: 2,
+      }}
+    >
+      <Container maxWidth={false}>{children}</Container>
+    </Box>
+  </>
+)
+
+export default Page
