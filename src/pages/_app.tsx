b9514be93f2403f9ae80f3bb817553afd9ec6d03
Ganesh Basa <basaganesh1@gmail.com>
2024-06-20 12:58:06 +0530

    Added Dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import { FC } from 'react'
 import { AppProps } from 'next/app'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { CssBaseline } from '@mui/material'
 
 import '@styles/globals.css'
 
 
-const App : FC<AppProps> = ({ Component, pageProps }) => <Component { ...pageProps } />
+const darkTheme = createTheme({ palette: { mode: 'dark' } })
+
+
+const App : FC<AppProps> = ({ Component, pageProps }) => (
+  <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+    <Component { ...pageProps } />
+  </ThemeProvider>
+)
 
 
 export default App
